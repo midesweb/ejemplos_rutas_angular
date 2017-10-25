33adc2cdff1d33dacc671c4f153c5628a5d86469
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2017-10-25 23:47:34 +0200

    Un tercer componente para enrutar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 
 const rutas: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'contacto', component: ContactoComponent }
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'quienessomos', component: QuienesSomosComponent },  
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactoComponent
+    ContactoComponent,
+    QuienesSomosComponent
   ],
   imports: [
     BrowserModule,
